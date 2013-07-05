174ecab963a364a77da04903ee0da063a708d3cf
Sam Stefan <sam@synthmedia.co.uk>
2013-07-05 15:20:48 +0100

    Init commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+Synth Media's Static Jade & Stylus Website Template
+=======
+
+A static website template that uses stylus, jade and live reload.
+
+Installation
+------------
+
+Dependency:
+  - [node.js](http://nodejs.org)
+  - [npm.js](https://npmjs.org)
+  - [grunt.js](http://gruntjs.com)
+
+Run
+---
+
+```
+git clone git@github.com:synthmedia/
+npm install
+grunt
+```
---
+++ grunt.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  var config =
+      { reload:
+        { port: 35729 // LR default
+        , liveReload: {}
+        }
+      , stylus:
+        { compile:
+          { options: { compress: true }
+          , files: { 'build/assets/css/global.css': 'app/stylus/main.styl' }
+          }
+        }
+      , jade:
+        { compile:
+          { options:
+            { data:
+              { debug: false
+              , compress: true
+              }
+            }
+          , files:
+            // Add in your jade template files here.
+            { 'build/index.html': [ 'app/templates/index.jade' ] }
+          }
+        }
+      , watch:
+        { stylus:
+          { files: 'app/stylus/*.styl'
+          , tasks: 'stylus reload'
+          }
+        , jade:
+          { files: ['app/templates/*.jade', 'app/templates/*/*.jade']
+          , tasks: 'jade reload'
+          }
+        }
+      }
+
+  grunt.initConfig(config)
+
+  // Load tasks from "grunt-sample" grunt plugin installed via Npm.
+  grunt.loadNpmTasks('grunt-contrib-stylus')
+  grunt.loadNpmTasks('grunt-contrib-jade')
+  grunt.loadNpmTasks('grunt-reload')
+
+  // Default task.
+  grunt.registerTask('default', ['stylus', 'jade', 'watch'])
+}
